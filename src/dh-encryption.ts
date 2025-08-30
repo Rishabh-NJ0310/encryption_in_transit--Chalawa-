3afdd7a2e34a8bb035b93526dc0f3c7755a3f6bd
Rishabh <rgmaheshwari06@gmail.com>
2025-08-30 16:01:45 +0530

    Refactor dhEncrypt function to remove debug logging and clean up code; add empty type definition files

--- src/dh-encryption.ts
+++ src/dh-encryption.ts
@@ -17,13 +17,8 @@ export function dhEncrypt({ plainText, sharedSecret }: DHEncryptionInput): strin
     let encrypted = cipher.update(plainText, 'utf-8', 'base64');
     encrypted += cipher.final('base64');
     const authTag = cipher.getAuthTag();
-    console.log("Encrypted:", encrypted);
-    console.log("IV:", Buffer.from(iv).toString('base64'));
-    console.log("Auth Tag:", authTag.toString('base64'));
-    const str = `${encrypted}+ ${Buffer.from(iv).toString('base64')}+ ${authTag.toString('base64')}`
-    console.log("Full Encrypted String:", str + " length of " + str.length);
-    return encrypted + ":" + Buffer.from(iv).toString('base64') + ":" + authTag.toString('base64');
     
+    return encrypted + ":" + Buffer.from(iv).toString('base64') + ":" + authTag.toString('base64');
 }
 
 /**
@@ -52,12 +47,3 @@ export function dhDecrypt({ encryptedText, sharedSecret }: DHDecryptionInput): a
     
     return JSON.parse(decrypted);
 }
-
-
-
-/*
-
-    CUSTOM PADDING MIDDLEWARE
-
-
-*/
